ae71b6725e85b79482dbc4580caff14f1a8c7aac
Thant2004Zin` <frequency307@gmail.com>
2022-05-31 16:02:44 +0630

    takuku

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.scss'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+let carouselControPre = document.getElementById("2carouselControPre");
+let carouselControNext = document.getElementById("2carouselControNext");
+
+
+carouselControPre.addEventListener('click',function () {
+    document.getElementById("HiddenBtnPre").click();
+});
+carouselControNext.addEventListener('click',function () {
+    document.getElementById("HiddenBtnNext").click();
+});
